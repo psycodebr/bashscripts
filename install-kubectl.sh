a9d39f2f2aa4ff77503f0933c0b1c114fcc18b87
viniciusboscolodnx <vinicius.boscolo@dnxbrasil.com>
2023-01-18 11:58:52 -0300

    install kubectl on linux

---
+++ install-kubectl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client --output=yaml
